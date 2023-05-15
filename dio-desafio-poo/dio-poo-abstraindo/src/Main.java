3c7bae2a2779ea5e7ad1dfab4f21173ca111b5d5
Rafael <rafaelmsilva@live.com>
2023-05-15 09:57:28 -0300

    curso e mentoria

--- dio-desafio-poo/dio-poo-abstraindo/src/Main.java
+++ dio-desafio-poo/dio-poo-abstraindo/src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.poo.dominio.Curso;
+import br.com.dio.desafio.poo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Java");
+        curso.setDescricao("curso de java");
+        curso.setCargaHoraria(8);
+        System.out.println( curso );
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Kotlin");
+        curso1.setDescricao("Curso de kotlin");
+        curso1.setCargaHoraria(6);
+        System.out.println( curso1 );
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Javascript");
+        mentoria.setDescricao("curso de javascript");
+        mentoria.setData(LocalDate.now());
+        System.out.println( mentoria );
     }
 }
