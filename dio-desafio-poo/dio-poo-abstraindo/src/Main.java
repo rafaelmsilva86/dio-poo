7c2ca957d0ab6c9ea4d3331640b2b95f093bc316
Rafael <rafaelmsilva@live.com>
2023-05-15 16:52:28 -0300

    final dio

--- dio-desafio-poo/dio-poo-abstraindo/src/Main.java
+++ dio-desafio-poo/dio-poo-abstraindo/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.poo.dominio.Bootcamp;
 import br.com.dio.desafio.poo.dominio.Curso;
+import br.com.dio.desafio.poo.dominio.Dev;
 import br.com.dio.desafio.poo.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,5 +25,34 @@ public class Main {
         mentoria.setDescricao("curso de javascript");
         mentoria.setData(LocalDate.now());
         System.out.println( mentoria );
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp java developer");
+        bootcamp.setDescricao("Descrição bootcamp java developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRafael = new Dev();
+        devRafael.setNome("Rafael");
+        System.out.println("Conteudos inscritos Rafael " + devRafael.getConteudosInseridos());
+        devRafael.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Rafael " + devRafael.getConteudosInseridos());
+        System.out.println("Conteudos concluidos Rafael " + devRafael.getConteudosConcluidos());
+        System.out.println("XP: " + devRafael.calcularTotalXp());
+        System.out.println("XP: " + devRafael.calcularTotalXp());
+
+        System.out.println("-----------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Joao" + devJoao.getConteudosInseridos());
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Joao" + devJoao.getConteudosInseridos());
+        System.out.println("Conteudos concluidos Joao" + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
     }
 }
